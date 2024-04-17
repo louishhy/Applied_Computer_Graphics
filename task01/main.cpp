fdee3d73b1934f6e2e0832fedc9896908ec6bbb3
MuFeng5557 <breezelo2000@gmail.com>
2024-04-17 10:35:30 +0900

    Minor fix: M_PI
    Altered the structure of M_PI definition according to instructor instruction on slack.

--- task01/main.cpp
+++ task01/main.cpp
@@ -8,7 +8,9 @@
 #include "stb_image_write.h"
 #include <cmath>
 // Mathematical constant definition for the constant difference across win and linux.
-#define M_PI 3.14159265358979323846
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288
+#endif
 
 /**
  * @brief compute the area of a triangle
